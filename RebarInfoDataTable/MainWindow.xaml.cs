2093a04ef20d9165f1706f8c86967556fb8ac8d0
Adrian Rymarz <adrian.rymarz1@gmail.com>
2023-03-05 13:14:58 +0100

    Init commit

---
+++ RebarInfoDataTable/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace RebarInfoDataTable
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
